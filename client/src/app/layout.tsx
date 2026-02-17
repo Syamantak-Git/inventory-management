702986e6c7da333036ccc8469c1145f4d1c56810
syamantak <132641600+Syamantak-Git@users.noreply.github.com>
2026-02-17 12:04:38 +0530

    restart project from scratch

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import DashboardWrapper from "./dashboardWrapper";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,11 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
-        <DashboardWrapper>
-          {children}
-        </DashboardWrapper>
-        
+        {children}
       </body>
     </html>
   );
